4ae5d2bba769141e3ba5fc7e5e57e79763bb2a3f
kudpig <shin.kudo.aa@gmail.com>
2021-03-05 16:56:04 +0900

    segmentControl

--- segmentedControl-Practice/ViewController.swift
+++ segmentedControl-Practice/ViewController.swift
@@ -8,12 +8,35 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var segmentControl: UISegmentedControl!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        segmentControl.backgroundColor = .white
+        view.backgroundColor = .darkGray
     }
 
+    @IBAction func didChangeSegment(_ sender: UISegmentedControl) {
+        if sender.selectedSegmentIndex == 0 {
+            view.backgroundColor = .darkGray
+        }
+        else if sender.selectedSegmentIndex == 1 {
+            view.backgroundColor = .red
+        }
+        else if sender.selectedSegmentIndex == 2 {
+            view.backgroundColor = .orange
+        }
+        else if sender.selectedSegmentIndex == 3 {
+            view.backgroundColor = .green
+        }
+        else if sender.selectedSegmentIndex == 4 {
+            view.backgroundColor = .blue
+        }
+        
+    }
+    
 
 }
 
